97229777b80520d18b072b73773400df2d2cb24f
waseem-abbas786 <waseem.abbas@example.com>
2026-01-28 17:33:36 +0500

    updating view

--- myproject/studentsapp/views.py
+++ myproject/studentsapp/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def std(request):
     return render(request, 'studentsapp/home.html')
+
+def detail(request):
+    return render(request, 'studentsapp/detail.html')
