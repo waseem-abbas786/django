980cd3b3dcfa2542bc14b5f328ef75bf77b18c81
waseem-abbas786 <waseem.abbas@example.com>
2026-01-28 17:52:52 +0500

    add html

--- myproject/studentsapp/urls.py
+++ myproject/studentsapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from studentsapp.views import std
+from studentsapp.views import std, about
 
 urlpatterns = [
     path('', std, name='std'),
+    path('about/', about, name='about'),
 ]
