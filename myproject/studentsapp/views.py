249e8c8f1d49874ecbb285a017f96fcc6caca65f
waseem-abbas786 <waseem.abbas@example.com>
2026-01-28 17:34:51 +0500

    addd

--- myproject/studentsapp/views.py
+++ myproject/studentsapp/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def std(request):
     return render(request, 'studentsapp/home.html')
+
+def about(request):
+    return render(request, 'studentsapp/about.html')
