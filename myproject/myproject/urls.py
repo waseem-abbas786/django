ad1b64ef74fcb3dc35154986d9b8e7e39b73e471
waseem-abbas786 <waseem.abbas@example.com>
2026-01-28 17:29:03 +0500

    Update urls.py routing

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('student/', include('studentsapp.urls')),
+    path('student/ ', include('studentsapp.urls')),
     path('admin/', admin.site.urls),
 ]
